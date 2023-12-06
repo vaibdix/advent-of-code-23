45a8215a41e69b3aad26d15a2ae605b815da521c
vaibdix <dixitvaib@gmail.com>
2023-12-07 02:08:46 +0530

    day-3 part-1 solution

---
+++ day-3/index.js
@@ -0,0 +1,49 @@
+import { readFileSync } from 'fs';
+
+const input = readFileSync('./input.txt', 'utf8').trimEnd();
+
+const dirs = [
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, -1],
+  [0, 1],
+  [1, -1],
+  [1, 0],
+  [1, 1],
+];
+
+function solve1(input) {
+    const map = input.split('\n').map((line) => line.split(''));
+    let sum = 0;
+    for (let i = 0; i < map.length; i++) {
+      for (let j = 0; j < map[i].length; j++) {
+        if (/[^\d^.]/.test(map[i][j])) {
+          for (let [di, dj] of dirs) {
+            if (/\d/.test(map[i + di][j + dj])) {
+              const digits = [map[i + di][j + dj]];
+              for (let j2 = j + dj - 1; j2 >= 0; j2--) {
+                if (/\d/.test(map[i + di][j2])) {
+                  digits.unshift(map[i + di][j2]);
+                  map[i + di][j2] = '.';
+                } else {
+                  break;
+                }
+              }
+              for (let j2 = j + dj + 1; j2 < map[i + di].length; j2++) {
+                if (/\d/.test(map[i + di][j2])) {
+                  digits.push(map[i + di][j2]);
+                  map[i + di][j2] = '.';
+                } else {
+                  break;
+                }
+              }
+              sum += +digits.join('');
+            }
+          }
+        }
+      }
+    }
+    console.log(sum);
+  }
+  solve1(input);
