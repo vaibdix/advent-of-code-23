37d1c03519a31354d030978be7753e5359900dbf
vaibdix <dixitvaib@gmail.com>
2023-12-11 00:59:32 +0530

    day-6 solution both parts

---
+++ day-6/index.js
@@ -0,0 +1,23 @@
+import { readFileSync } from 'fs';
+
+const input = readFileSync('input.txt', 'utf8').trimEnd();
+
+function solve(input, part) {
+  const [times, dists] =
+    part === 1
+      ? input.split('\n').map((line) => line.match(/\d+/g).map(Number))
+      : input.split('\n').map((line) => [+line.match(/\d+/g).join('')]);
+
+  let product = 1;
+  for (let i = 0; i < times.length; i++) {
+    const time = times[i];
+    const dist = dists[i];
+
+    const minTime = Math.floor((time - Math.sqrt(time ** 2 - 4 * dist)) / 2);
+    const nWays = time - 2 * minTime - 1;
+    product *= nWays;
+  }
+  console.log(product);
+}
+solve(input, 1);
+solve(input, 2);
