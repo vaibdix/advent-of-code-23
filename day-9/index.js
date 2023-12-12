5fb47f9992deb4e2763f3f64185ac16a962f621d
vaibdix <dixitvaib@gmail.com>
2023-12-12 18:32:31 +0530

    day-9 solutions

---
+++ day-9/index.js
@@ -0,0 +1,26 @@
+import { readFileSync } from 'fs';
+
+const input = readFileSync('input.txt', 'utf8').trimEnd();
+
+function solve1(input) {
+  let sum = 0;
+  for (const line of input.split('\n')) {
+    let curr = line.match(/[-\d]+/g).map(Number);
+    const rows = [curr];
+    while (curr.some(Boolean)) {
+      const next = [];
+      for (let i = 0; i < curr.length - 1; i++) {
+        next.push(curr[i + 1] - curr[i]);
+      }
+      curr = next;
+      rows.push(next);
+    }
+    let carry = 0;
+    for (let i = rows.length - 2; i >= 0; i--) {
+      carry += rows[i].at(-1);
+    }
+    sum += carry;
+  }
+  console.log(sum);
+}
+solve1(input);
