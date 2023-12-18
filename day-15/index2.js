c2d1a83c63c7868a154dc0d13b4d23cd2007845a
vaibdix <dixitvaib@gmail.com>
2023-12-18 22:23:51 +0530

    part 2 d-15

---
+++ day-15/index2.js
@@ -0,0 +1,37 @@
+import { readFileSync } from 'node:fs';
+
+const input = readFileSync('input.txt', 'utf8').trimEnd();
+
+function hash(str) {
+    let val = 0;
+    for (const char of str) {
+      val += char.codePointAt(0);
+      val *= 17;
+      val %= 256;
+    }
+    return val;
+  }
+
+function solve2(input) {
+    const map = [...Array(256)].map(() => ({}));
+    for (const step of input.split(',')) {
+      let [, label, op, len] = step.match(/(\w+)([-=])(\d*)/);
+      len = +len;
+      const box = hash(label);
+      if (op === '=') {
+        map[box][label] = len;
+      } else {
+        delete map[box][label];
+      }
+    }
+  
+    let sum = 0;
+    for (let box = 0; box < map.length; box++) {
+      const lengths = Object.values(map[box]);
+      for (let slot = 0; slot < lengths.length; slot++) {
+        sum += (box + 1) * (slot + 1) * lengths[slot];
+      }
+    }
+    console.log(sum);
+  }
+  solve2(input);
